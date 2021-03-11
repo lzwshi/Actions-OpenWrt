6d1ce011a159ae0e7c02d1c34c76844bdb6aaab3
lzwshi <46265588+lzwshi@users.noreply.github.com>
2021-03-11 17:09:25 +0800

    update

--- diy-part-two.sh
+++ diy-part-two.sh
@@ -160,6 +160,5 @@ svn co https://github.com/immortalwrt/immortalwrt/branches/master/package/lean/l
 popd
 
 # This script is only for Raspberry Pi 4, Thanks @xmapst
-
 sed -i 's/36/44/g;s/VHT80/VHT20/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
