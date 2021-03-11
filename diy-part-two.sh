9357153850534b8d729e4b596a15afebf2f2db4a
lzwshi <46265588+lzwshi@users.noreply.github.com>
2021-03-11 12:46:19 +0800

    update

--- diy-part-two.sh
+++ diy-part-two.sh
@@ -143,6 +143,9 @@ pushd po2lmo
 make && sudo make install
 popd
 
+# Fix SDK
+sed -i '/$(SDK_BUILD_DIR)\/$(STAGING_SUBDIR_HOST)\/usr\/bin/d;/LICENSE/d' target/sdk/Makefile
+
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
